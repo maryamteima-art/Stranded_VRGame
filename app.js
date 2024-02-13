f11edd06a5bf0bdc316fbc3a1ac2a57b39b7c835
Maryam Teima <101683109+maryamteima-art@users.noreply.github.com>
2024-02-12 22:06:08 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+//SERVER HANDLING CODE
+
+const express   = require('express');
+const app       = express();
+const http      = require('http');
+const server    = require('http').createServer(app);  
+const io        = require('socket.io')(server);
+
+const LISTEN_PORT   = 8080;
+
+app.use(express.static(__dirname + '/public')); //set root path of server ...
+
+//our routes
+app.get( '/', function( req, res ){ 
+    res.sendFile( __dirname + '/public/index.html' );
+});
+
+app.get( '/2D', function( req, res ){ 
+    res.sendFile( __dirname + '/public/2D.html' );
+});
+
+app.get( '/3D', function( req, res ){ 
+    res.sendFile( __dirname + '/public/3D.html' );
+});
+
+server.listen(LISTEN_PORT);
+console.log("Listening on port: " + LISTEN_PORT );
